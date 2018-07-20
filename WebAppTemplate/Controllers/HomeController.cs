d2c3a391f9c604ed3742371caa3544bf7bc878c1
az1admin <az1admin@az1vs01>
2018-07-20 11:57:43 +0000

    Test commit

--- WebAppTemplate/Controllers/HomeController.cs
+++ WebAppTemplate/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebAppTemplate.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description test page .";
 
             return View();
         }
